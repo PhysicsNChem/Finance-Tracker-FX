914e15b387f30f97e56687cc0a130bd2c65786f6
PhysicsNChem <rohanasharma1@gmail.com>
2024-12-24 13:25:14 -0500

    Initial commit

--- Setup.java
+++ Setup.java
@@ -4,6 +4,17 @@ import java.awt.event.*;
 import javax.swing.*;
 public class Setup {
     public Setup() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         JFrame frame1 = new JFrame("FBLA Project Setup");
         frame1.setSize(800,600);
         frame1.setLocationRelativeTo(null);

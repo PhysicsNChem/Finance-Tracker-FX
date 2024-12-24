e0fb701ef7fa8eb9127c5ce1c95b60f3b91996f5
PhysicsNChem <rohanasharma1@gmail.com>
2024-12-24 12:24:32 -0500

    Initial commit

--- GUI.java
+++ GUI.java
@@ -5,7 +5,10 @@ import javax.swing.*;
 
 public class GUI {
     public GUI(){
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("FBLA Project");
+        JLabel label = new JLabel("Balance");
+        label.setLocation(50, 50);
+        frame.add(label);
         frame.setSize(1280,720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);

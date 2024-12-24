7e93564c283556c59d62628f8c0cded656c1d98c
PhysicsNChem <rohanasharma1@gmail.com>
2024-12-24 11:41:59 -0500

    Initial commit

--- GUI.java
+++ GUI.java
@@ -6,5 +6,8 @@ import javax.swing.*;
 public class GUI {
     public GUI(){
         JFrame frame = new JFrame();
+        frame.setSize(1280,720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }

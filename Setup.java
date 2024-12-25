843e1c0fa4999bc293fafe7f744aa691533587f5
PhysicsNChem <rohanasharma1@gmail.com>
2024-12-24 19:06:44 -0500

    Fixed buttons

--- Setup.java
+++ Setup.java
@@ -29,7 +29,7 @@ public class Setup extends JFrame implements ActionListener {
         RadioPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Add padding
 
         // Create and add a label
-        JLabel label = new JLabel("Select a langauge/Choissez votre langue:");
+        JLabel label = new JLabel("Select a langauge/Choisissez votre langue:");
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
         RadioPanel.add(label);
         RadioPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Add some spacing
@@ -45,20 +45,19 @@ public class Setup extends JFrame implements ActionListener {
         group.add(button1);
         group.add(button2);
 
-        // Add ActionListeners
+        //w
         button1.addActionListener(this);
         button2.addActionListener(this);
-
         // Add buttons to the panel
         RadioPanel.add(button1);
         RadioPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Add spacing between buttons
         RadioPanel.add(button2);
 
         //Add confirm button
-        JButton button3 = new JButton("Continue");
-        button3.addActionListener(this);
+        button3 = new JButton("Continue");
         button3.setAlignmentX(Component.CENTER_ALIGNMENT);
         RadioPanel.add(Box.createRigidArea(new Dimension(0, 250)));
+        button3.addActionListener(this);
         RadioPanel.add(button3);
         // Add panel to the frame
         add(RadioPanel);
@@ -68,16 +67,23 @@ public class Setup extends JFrame implements ActionListener {
     }
 
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button1) {
-            JOptionPane.showMessageDialog(this, "Option 1 selected!");
-        } else if (e.getSource() == button2) {
-            JOptionPane.showMessageDialog(this, "Option 2 selected!");
+        if (e.getSource() == button3) { // Check for "Continue" button click
+            if (button1.isSelected()) {
+                englishSelected = true;
+                LanguageSelected = true;
+            } else if (button2.isSelected()) {
+                LanguageSelected = true;
+            } else {
+                JOptionPane.showMessageDialog(this, "Please choose an option / Veuillez choisir une option", "Error", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
+
+
     public static void main(String[] args) {
         new Setup();
     }
+
 }
